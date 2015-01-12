d8d86510d761823ecdbdc0fce63aa34ee9c294d6
Purple Dragonfly <10949221@qq.com>
2015-01-12 18:07:32 +0800

    gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -7,31 +7,32 @@ var gulp = require('gulp'),//基础模块
     jshint = require('gulp-jshint'),//js语法检测
     uglify = require('gulp-uglify');//js压缩
 
+gulp.task('css', function(){
+    gulp.src('lib/less/index.less')
+        .pipe(less())
+        .pipe(gulp.dest('dist/css'));
+});
+
 gulp.task('jshint', function(){
-   gulp.src('src/js/*.js')
+   gulp.src('lib/js/*.js')
        .pipe(jshint())
        .pipe(jshint.reporter('default'));
 });
-gulp.task('css', function(){
-   gulp.src('src/less/index.less')
-       .pipe(less())
-       .pipe(gulp.dest('dest/css'));
-});
 
 gulp.task('script',function(){
-    gulp.src('src/js/*.js')
+    gulp.src('lib/js/*.js')
         .pipe(rename({extname:'.min.js'}))
         .pipe(uglify())
-        .pipe(gulp.dest('dest/js'))
+        .pipe(gulp.dest('dist/js'))
 });
 
 gulp.task('default',function(){
-    gulp.run('jshint','script');
     gulp.run('css');
-    gulp.watch('src/js/*.js', function(){
-        gulp.run('jshint','script');
-    });
-    gulp.watch('src/less/*.less', function(){
+    gulp.watch('lib/less/*.less', function(){
         gulp.run('css');
+    });
+    gulp.run('jshint','script');
+    gulp.watch('lib/js/*.js', function(){
+        gulp.run('jshint','script');
     });
 });
